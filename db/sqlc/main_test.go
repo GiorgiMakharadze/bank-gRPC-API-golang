1f3b460dab72cd65d0932f8d9108f95beb9e0227
GiorgiMakharadze <makharadzegiorgi00@gmail.com>
2023-09-21 16:33:45 -0400

    added random generator functions in utls & tested accounts with unit testing and testify

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,30 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:secret@localhost:5432/bank?sslmode=disable"
+)
+
+var testQueries *Queries
+
+func TestMain(m *testing.M) {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect database", err)
+	}
+
+	testQueries = New(conn)
+
+	m.Run()
+
+	os.Exit(m.Run())
+}
