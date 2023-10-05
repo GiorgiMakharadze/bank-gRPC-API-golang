e2d294df73cb81dbddcb8d53a3202361988301dd
GiorgiMakharadze <makharadzegiorgi00@gmail.com>
2023-10-05 17:41:26 -0400

    added getAcoount and account list

--- api/server.go
+++ api/server.go
@@ -17,6 +17,8 @@ func NewServer(store *db.Store) *Server {
 	router := gin.Default()
 
 	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.listAccount)
 
 	server.router = router
 	return server
