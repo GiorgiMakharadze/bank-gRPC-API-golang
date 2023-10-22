2e8264364e714a5b563b048d122f2b2007b07685
GiorgiMakharadze <makharadzegiorgi00@gmail.com>
2023-10-22 15:04:14 +0400

    refresh token

--- util/config.go
+++ util/config.go
@@ -14,6 +14,8 @@ type Config struct {
 	ServerAddress       string        `mapstructure:"SERVER_ADDRESS"`
 	TokenSymmetricKey   string        `mapstructure:"TOKEN_SYMMETRIC_KEY"`
 	AccessTokenDuration time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
+	RefreshTokenDuration time.Duration `mapstructure:"REFRESH_TOKEN_DURATION"`
+
 }
 
 // LoadConfig reads configuration from file or environment variables.
