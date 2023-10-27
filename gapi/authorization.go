b78618ab71e59b074b7961648b286ec5c3ba6b9a
GiorgiMakharadze <makharadzegiorgi00@gmail.com>
2023-10-27 12:39:22 +0400

    authorization user

---
+++ gapi/authorization.go
@@ -0,0 +1,45 @@
+package gapi
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/GiorgiMakharadze/bank-API-golang/token"
+	"google.golang.org/grpc/metadata"
+)
+
+const (
+	authorizationHeader       = "authorization"
+	authorizationHeaderBearer = "bearer"
+)
+
+func (server *Server) authorizeUser(ctx context.Context) (*token.Payload, error) {
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return nil, fmt.Errorf("missing metadata")
+	}
+
+	values := md.Get(authorizationHeader)
+	if len(values) == 0 {
+		return nil, fmt.Errorf("missing authorization header")
+	}
+
+	authHeader := values[0]
+	fields := strings.Fields(authHeader)
+	if len(fields) < 2 {
+		return nil, fmt.Errorf("invalid authorization header format")
+	}
+
+	authType := strings.ToLower(fields[0])
+	if authType != authorizationHeaderBearer {
+		return nil, fmt.Errorf("unsupported authorization type: %s", authType)
+	}
+
+	accessToken := fields[1]
+	payload, err := server.tokenMaker.VerifyToken(accessToken)
+	if err != nil {
+		return nil, fmt.Errorf("invalid access token: %s", err)
+	}
+	return payload, nil
+}
